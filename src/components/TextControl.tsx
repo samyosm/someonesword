7904ed6c80d295d7a21b38afbef3010b5897496c
Samy Rahmani <samy.osmium@gmail.com>
2022-10-27 19:42:02 -0400

    Stylex TextControl

--- src/components/TextControl.tsx
+++ src/components/TextControl.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React from "react";
 
-export const TextControl = () => {
+export const TextControl = ({
+  text,
+  readOnly = false,
+  onChange = () => {},
+  placeholder,
+}: {
+  text?: string;
+  readOnly?: boolean;
+  placeholder: string;
+  onChange?: (text: string) => void;
+}) => {
   return (
-    <div>TextControl</div>
-  )
-}
+    <textarea
+      readOnly={readOnly}
+      placeholder={placeholder}
+      rows={1}
+      style={{
+        height: readOnly ? "unset" : "16rem" 
+      }}
+      onChange={(event) => onChange(event.target.value)}
+      className="bg-[#f7fafb] resize-none rounded-2xl p-5 outline-none text-[#1d2329]"
+    >
+      {text}
+    </textarea>
+  );
+};
