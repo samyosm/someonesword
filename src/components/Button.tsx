a1063d607aac9e93d6cc4185a9d58163602b7f01
Samy Rahmani <samy.osmium@gmail.com>
2022-10-27 19:03:07 -0400

    Styled Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import { If, Then, Else } from "react-if";
 
-export const Button = () => {
+export const Button = ({
+  label,
+  onPress,
+  inverted = false,
+}: {
+  label: string;
+  onPress: () => void;
+  inverted?: boolean;
+}) => {
   return (
-    <div>Button</div>
-  )
-}
+    <If condition={!inverted}>
+      <Then>
+        <button className="bg-[#4c7cff] p-5 rounded-xl" onClick={onPress}>
+          <p className="text-[#f7fafb]">{label}</p>
+        </button>
+      </Then>
+      <Else>
+        <button className="bg-[#f7fafb] p-5 rounded-xl" onClick={onPress}>
+          <p className="text-[#4c7cff]">{label}</p>
+        </button>
+      </Else>
+    </If>
+  );
+};
